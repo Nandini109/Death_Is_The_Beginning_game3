5415ae86fa7faa505e1277b0194c01231a9f4a07
Eni <nandiniparmar7781@gmail.com>
2024-10-09 07:03:13 +0530

    Speed Change in Water

--- Assets/Code/Scripts/Mechanics/DrowningWater.cs
+++ Assets/Code/Scripts/Mechanics/DrowningWater.cs
@@ -6,22 +6,26 @@ using UnityEngine;
 public class DrowningWater : MonoBehaviour
 {
     public LayerMask playerLayer;
-    [SerializeField] float DrownTime = 3f;
-    [SerializeField] float WaterLevel = 0f;
+    [SerializeField] private float drownTime = 3f;
+    [SerializeField] private float waterLevel = 0f;
+    [SerializeField] private float speedInWater = 0.5f;
     private Coroutine drowningCoroutine;
+    private float originalSpeed;
     private IEnumerator CheckWaterLevel(Collider2D player)
     {
         PlayerController playerController = player.GetComponent<PlayerController>();
-
+        originalSpeed = playerController.Data.MovementSpeed;
+        playerController.Data.MovementSpeed *= speedInWater;
         while (true)
         {
-            bool isFullyInsideWater = player.bounds.min.y < WaterLevel && player.bounds.max.y < WaterLevel;
+
+            bool isFullyInsideWater = player.bounds.min.y < waterLevel && player.bounds.max.y < waterLevel;
 
             if (isFullyInsideWater) // is the player is inside water
             {
-                yield return new WaitForSeconds(DrownTime);
+                yield return new WaitForSeconds(drownTime);
 
-                if (player.bounds.min.y < WaterLevel && player.bounds.min.y < WaterLevel)
+                if (player.bounds.min.y < waterLevel && player.bounds.min.y < waterLevel)
                 {
                     Debug.Log("Player Drownedddd!!!");
                     playerController.Death(); //if the player is inisde water for more than 3f they die
@@ -40,8 +44,10 @@ public class DrowningWater : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            
             Debug.Log("Player is in the water");
             drowningCoroutine = StartCoroutine(CheckWaterLevel(collision));
+
         }
     }
 
@@ -49,14 +55,16 @@ public class DrowningWater : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
             if (drowningCoroutine != null)
             {
                 Debug.Log("Player is out od water");
                 StopCoroutine(drowningCoroutine);
                 drowningCoroutine = null;
-
             }
+            //Change the speed back to original
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            playerController.Data.MovementSpeed = originalSpeed;
+            Debug.Log(originalSpeed);
         }
     }
 }
