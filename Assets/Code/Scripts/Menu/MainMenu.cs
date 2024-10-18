881de65dd4e405dcb2519b4b69cc39edbd1a861c
Nibour16 <ruobinh3@hotmail.com>
2024-10-18 07:56:31 -0600

    By default the menus are focusing on my level. If you choose the old one, make sure to reset all of the menus to start the level as "Level". Just don't forget about this.

--- Assets/Code/Scripts/Menu/MainMenu.cs
+++ Assets/Code/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("Level (Revolution)");
     }
 
     public void GoToSettingsMenu()
